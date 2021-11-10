463045b79d2a5aef5243d3bc20c0f4759a849772
aterui <hanabi0111@gmail.com>
2021-11-10 15:56:23 -0500

    add: practice files

---
+++ run_exercise_bayes.R
@@ -0,0 +1,8 @@
+# 1st exercise
+# use `iris` data
+# response: Y would be Sepal.Length
+# explanatory: X would be Sepal.Width and Petal.Length
+
+# 2nd exercise
+# add random effect of species
+# use half student_t distribution for priors of sds
